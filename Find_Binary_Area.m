08ef3524528e05842dfbad1ca483621218cdd8cc
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2022-10-26 16:51:02 +0200

    Find area of segmented object
    This .m file requires a binary image as input and displays the number of pixels that have a binary equivalent of 1.

---
+++ Find_Binary_Area.m
@@ -0,0 +1,5 @@
+%%
+%imtool(BWL1)
+I1 = imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\LeftCamera\GX010009-frames\BWL.jpg');
+imtool(I1)
+bwarea(I1)
