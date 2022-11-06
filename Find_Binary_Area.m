9628bbcfa6fd90b0721269e3ce749ec64d9b0ead
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2022-11-06 08:44:15 +0200

    Undated comments in file

--- Find_Binary_Area.m
+++ Find_Binary_Area.m
@@ -1,5 +1,8 @@
-%%
-%imtool(BWL1)
+% Read an image from the file specified by filename, inferring the format of the file from its contents.
 I1 = imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\LeftCamera\GX010009-frames\BWL.jpg');
+
+% Display a image from a file I1.
 imtool(I1)
+
+% Estimates the area of the objects in binary image I1.
 bwarea(I1)
