63b4f6faeae245a991c86a0abc10edda61f9b72b
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2022-11-06 09:23:26 +0200

    Update Read_Stereo_Pixel_Coordinates.m

--- Read_Stereo_Pixel_Coordinates.m
+++ Read_Stereo_Pixel_Coordinates.m
@@ -1,5 +1,4 @@
 %% Load images to visualise pixel co-ordinates for left and right images taken with stereo setup.
-
 I1 = im2gray(imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\LeftCamera\GX010007-frames\frame0-00-01.00.jpg'));
 I2 = im2gray(imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\RightCamera\GX010190-frames\frame0-00-01.00.jpg'));
 
