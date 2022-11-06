befcc916db1ebe23fe0492e53b1941cf04358f84
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2022-11-06 09:23:03 +0200

    Update Read_Stereo_Pixel_Coordinates.m

--- Read_Stereo_Pixel_Coordinates.m
+++ Read_Stereo_Pixel_Coordinates.m
@@ -3,5 +3,6 @@
 I1 = im2gray(imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\LeftCamera\GX010007-frames\frame0-00-01.00.jpg'));
 I2 = im2gray(imread('C:\Users\Donovan Da Silva\OneDrive - University of Cape Town\University\Fourth Year\EEE4022S\Experiment\RightCamera\GX010190-frames\frame0-00-01.00.jpg'));
 
+% Opens Image Viewer and displays the image file with file name I1 and I2. The bottom of the screen displays the pixel co-ordinates as you move the mouse over the image.
 imtool(I1)
 imtool(I2)
